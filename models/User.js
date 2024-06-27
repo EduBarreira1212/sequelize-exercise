1c17b582b650a621efcd62f988a29836a8174c87
Eduardo Barreira <edubarreira1212@gmail.com>
2024-06-26 22:40:56 -0300

    feat: user table file created

---
+++ models/User.js
@@ -0,0 +1,15 @@
+import sequelize from "../config/database";
+import { DataTypes } from "sequelize";
+
+const User = sequelize.define("User", {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING
+});
+
+export default User;
