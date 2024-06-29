80b14dc792432560637a8c34cd28aab6fa34a1d2
Eduardo Barreira <edubarreira1212@gmail.com>
2024-06-29 17:54:02 -0300

    feat: user's new crud created

--- src/index.js
+++ src/index.js
@@ -8,21 +8,26 @@ try {
     await User.sync();
 
     const newUser = await User.create({
-        name: "Eduardo",
         email: "edu@.com",
+        name: "Eduardo",
         password: "12345",
+        phone: "(85)11111-2222",
+        adress: "Fortaleza-CE"
     });
-    console.log("New user created:", newUser.toJSON());
 
-    const allUsers = await User.findAll();
-    console.log("All users:", allUsers.map((u) => u.toJSON()));
+    console.log("New user:", newUser.toJSON());
 
-    const user = await User.findByPk(1);
-    await user.update({password: "00000"});
+    const users = await User.findAll();
+    console.log("Users list:", users.map((u) => u.toJSON()));
+
+    const user = await User.findByPk("edu@.com");
+    await user.update({
+        name: "Eduardo Barreira"
+    });
     console.log("User updated:", user.toJSON());
 
-    const userDeleted = await User.findByPk(1);
-    await userDeleted.destroy();
+    const userDelete = await User.findByPk("edu@.com");
+    await userDelete.destroy();
     console.log("User deleted");
 } catch (error) {
     console.log("Error to conect/insert on database:", error);
