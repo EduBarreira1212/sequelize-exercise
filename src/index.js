e7d17485c4bc044e71bad8205c50a51e8dc06aa2
Eduardo Barreira <edubarreira1212@gmail.com>
2024-06-29 16:24:03 -0300

    feat: crud created to test db

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 import sequelize from "../config/database.js";
+import User from "../models/User.js";
 
 try {
-    console.log("TRY");
     await sequelize.authenticate();
     console.log("Database conected");
 
+    await User.sync();
+
+    const newUser = await User.create({
+        name: "Eduardo",
+        email: "edu@.com",
+        password: "12345",
+    });
+    console.log("New user created:", newUser.toJSON());
+
+    const allUsers = await User.findAll();
+    console.log("All users:", allUsers.map((u) => u.toJSON()));
+
+    const user = await User.findByPk(1);
+    await user.update({password: "00000"});
+    console.log("User updated:", user.toJSON());
+
+    const userDeleted = await User.findByPk(1);
+    await userDeleted.destroy();
+    console.log("User deleted");
 } catch (error) {
-    console.log("ERRO");
     console.log("Error to conect/insert on database:", error);
 } finally {
     console.log("CLOSE");
